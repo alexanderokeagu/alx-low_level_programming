1bfba8cfaa7ccd6e0a06174ab11b7ecb54794852
alexanderokeagu <pezokeagu@gmail.com>
2022-06-30 13:45:09 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints out the last part of a quote in the standard error.
  * Return: Always 1 (success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korper, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful - Dora Korper, 2015-10-19\n", 58);
 	return (1);
 }
