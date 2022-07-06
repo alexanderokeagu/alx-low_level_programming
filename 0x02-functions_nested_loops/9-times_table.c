824943f7614aee662f831812ca844d17d9e74db1
alexanderokeagu <pezokeagu@gmail.com>
2022-07-06 03:50:19 -0700

    Add 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,7 +26,7 @@ void times_table(void)
 				_putchar(k + '0');
 			}	else if (k >= 10)
 			{
-				putchar(',');
+				_putchar(',');
 				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
