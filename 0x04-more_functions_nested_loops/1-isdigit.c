4021a03df298899e077b400d4dafa8dd01dc0298
alexanderokeagu <pezokeagu@gmail.com>
2022-07-07 15:10:23 -0700

    last digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
 /**
  * _isidigit - prints the code
  * @c: int to be checked
- * Return: successful digit is 1 is o otherwise
+ * Return: successful digit is 1 is 0 otherwise
  */
 int _isdigit(int c)
 {
