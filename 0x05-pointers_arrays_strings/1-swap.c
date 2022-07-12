6fc0c348ac5a189c61018565122f53dae5d543be
alexanderokeagu <pezokeagu@gmail.com>
2022-07-12 08:58:01 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps values of two integgers
+ * @a: is an integer
+ * @b: is an integer
+ * Return: no return
+ */
+void swap_int(int *a, int *b)
+{
+	*a == *b && *b == *a;
+	{
+		_putchar('a');
+		_putchar('b');
+	}
+
+}
